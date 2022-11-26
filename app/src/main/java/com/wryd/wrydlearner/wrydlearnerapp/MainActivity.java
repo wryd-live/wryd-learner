0b216b230877d4ef8094e8c2459366a506da37bd
SIDDHANT DIXIT <siddhant.dixit23@gmail.com>
2022-11-26 16:21:02 +0530

    Added Files

---
+++ app/src/main/java/com/wryd/wrydlearner/wrydlearnerapp/MainActivity.java
@@ -0,0 +1,377 @@
+package com.wryd.wrydlearner.wrydlearnerapp;
+
+import android.Manifest;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.SystemClock;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.text.util.Linkify;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
+import org.json.JSONObject;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    // logging
+    private final String TAG = "MainActivity";
+
+
+    // background manager
+    private PendingIntent recurringLl24 = null;
+    private Intent ll24 = null;
+    AlarmManager alarms = null;
+    WebSocketClient mWebSocketClient = null;
+    Timer timer = null;
+    private RemindTask oneSecondTimer = null;
+
+    private String[] autocompleteLocations = new String[] {"bedroom","living room","kitchen","bathroom", "office"};
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "MainActivity onDestroy()");
+        if (alarms != null) alarms.cancel(recurringLl24);
+        if (timer != null) timer.cancel();
+        if (mWebSocketClient != null) {
+            mWebSocketClient.close();
+        }
+        android.app.NotificationManager mNotificationManager = (android.app.NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.cancel(0);
+        Intent scanService = new Intent(this, ScanService.class);
+        stopService(scanService);
+        super.onDestroy();
+    }
+
+    class RemindTask extends TimerTask {
+        private Integer counter = 0;
+
+        public void resetCounter() {
+            counter = 0;
+        }
+        public void run() {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    counter++;
+                    if (mWebSocketClient != null) {
+                        if (mWebSocketClient.isClosed()) {
+                            connectWebSocket();
+                        }
+                    }
+                    TextView rssi_msg = (TextView) findViewById(R.id.textOutput);
+                    String currentText = rssi_msg.getText().toString();
+                    if (currentText.contains("ago: ")) {
+                        String[] currentTexts = currentText.split("ago: ");
+                        currentText = currentTexts[1];
+                    }
+                    rssi_msg.setText(counter + " seconds ago: " + currentText);
+                }
+            });
+        }
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // check permissions
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.WAKE_LOCK, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.INTERNET, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.BLUETOOTH, Manifest.permission.BLUETOOTH_ADMIN, Manifest.permission.CHANGE_WIFI_STATE, Manifest.permission.ACCESS_WIFI_STATE}, 1);
+        }
+
+        TextView rssi_msg = (TextView) findViewById(R.id.textOutput);
+        rssi_msg.setText("not running");
+
+
+        // check to see if there are preferences
+        SharedPreferences sharedPref = MainActivity.this.getPreferences(Context.MODE_PRIVATE);
+        EditText familyNameEdit = (EditText) findViewById(R.id.familyName);
+        familyNameEdit.setText(sharedPref.getString("familyName", ""));
+        EditText deviceNameEdit = (EditText) findViewById(R.id.deviceName);
+        deviceNameEdit.setText(sharedPref.getString("deviceName", ""));
+        EditText serverAddressEdit = (EditText) findViewById(R.id.serverAddress);
+        serverAddressEdit.setText(sharedPref.getString("serverAddress", ((EditText) findViewById(R.id.serverAddress)).getText().toString()));
+        CheckBox checkBoxAllowGPS = (CheckBox) findViewById(R.id.allowGPS);
+        checkBoxAllowGPS.setChecked(sharedPref.getBoolean("allowGPS",false));
+
+
+        AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.locationName);
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, autocompleteLocations);
+        textView.setAdapter(adapter);
+
+
+        ToggleButton toggleButtonTracking = (ToggleButton) findViewById(R.id.toggleScanType);
+        toggleButtonTracking.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                TextView rssi_msg = (TextView) findViewById(R.id.textOutput);
+                rssi_msg.setText("not running");
+                Log.d(TAG, "toggle set to false");
+                if (alarms != null) alarms.cancel(recurringLl24);
+                android.app.NotificationManager mNotificationManager = (android.app.NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                mNotificationManager.cancel(0);
+                if (timer != null) timer.cancel();
+
+                CompoundButton scanButton = (CompoundButton) findViewById(R.id.toggleButton);
+                scanButton.setChecked(false);
+            }
+        });
+
+        ToggleButton toggleButton = (ToggleButton) findViewById(R.id.toggleButton);
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    TextView rssi_msg = (TextView) findViewById(R.id.textOutput);
+                    String familyName = ((EditText) findViewById(R.id.familyName)).getText().toString().toLowerCase();
+                    if (familyName.equals("")) {
+                        rssi_msg.setText("family name cannot be empty");
+                        buttonView.toggle();
+                        return;
+                    }
+
+                    String serverAddress = ((EditText) findViewById(R.id.serverAddress)).getText().toString().toLowerCase();
+                    if (serverAddress.equals("")) {
+                        rssi_msg.setText("server address cannot be empty");
+                        buttonView.toggle();
+                        return;
+                    }
+                    if (serverAddress.contains("http")!=true) {
+                        rssi_msg.setText("must include http or https in server name");
+                        buttonView.toggle();
+                        return;
+                    }
+                    String deviceName = ((EditText) findViewById(R.id.deviceName)).getText().toString().toLowerCase();
+                    if (deviceName.equals("")) {
+                        rssi_msg.setText("device name cannot be empty");
+                        buttonView.toggle();
+                        return;
+                    }
+                    boolean allowGPS = ((CheckBox) findViewById(R.id.allowGPS)).isChecked();
+                    Log.d(TAG,"allowGPS is checked: "+allowGPS);
+                    String locationName = ((EditText) findViewById(R.id.locationName)).getText().toString().toLowerCase();
+
+                    CompoundButton trackingButton = (CompoundButton) findViewById(R.id.toggleScanType);
+                    if (trackingButton.isChecked() == false) {
+                        locationName = "";
+                    } else {
+                        if (locationName.equals("")) {
+                            rssi_msg.setText("location name cannot be empty when learning");
+                            buttonView.toggle();
+                            return;
+                        }
+                    }
+
+                    SharedPreferences sharedPref = MainActivity.this.getPreferences(Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPref.edit();
+                    editor.putString("familyName", familyName);
+                    editor.putString("deviceName", deviceName);
+                    editor.putString("serverAddress", serverAddress);
+                    editor.putString("locationName", locationName);
+                    editor.putBoolean("allowGPS",allowGPS);
+                    editor.commit();
+
+                    rssi_msg.setText("running");
+                    // 24/7 alarm
+                    ll24 = new Intent(MainActivity.this, AlarmReceiverLife.class);
+                    Log.d(TAG, "setting familyName to [" + familyName + "]");
+                    ll24.putExtra("familyName", familyName);
+                    ll24.putExtra("deviceName", deviceName);
+                    ll24.putExtra("serverAddress", serverAddress);
+                    ll24.putExtra("locationName", locationName);
+                    ll24.putExtra("allowGPS",allowGPS);
+                    recurringLl24 = PendingIntent.getBroadcast(MainActivity.this, 0, ll24, PendingIntent.FLAG_CANCEL_CURRENT);
+                    alarms = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+                    alarms.setRepeating(AlarmManager.RTC_WAKEUP, SystemClock.currentThreadTimeMillis(), 60000, recurringLl24);
+                    timer = new Timer();
+                    oneSecondTimer = new RemindTask();
+                    timer.scheduleAtFixedRate(oneSecondTimer, 1000, 1000);
+                    connectWebSocket();
+
+                    String scanningMessage = "Scanning for " + familyName + "/" + deviceName;
+                    if (locationName.equals("") == false) {
+                        scanningMessage += " at " + locationName;
+                    }
+                    NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(MainActivity.this)
+                            .setSmallIcon(R.drawable.ic_stat_name)
+                            .setContentTitle(scanningMessage)
+                            .setContentIntent(recurringLl24);
+                    //specifying an action and its category to be triggered once clicked on the notification
+                    Intent resultIntent = new Intent(MainActivity.this, MainActivity.class);
+                    resultIntent.setAction("android.intent.action.MAIN");
+                    resultIntent.addCategory("android.intent.category.LAUNCHER");
+                    PendingIntent resultPendingIntent = PendingIntent.getActivity(MainActivity.this, 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+                    notificationBuilder.setContentIntent(resultPendingIntent);
+
+                    android.app.NotificationManager notificationManager =
+                            (android.app.NotificationManager) MainActivity.this.getSystemService(Context.NOTIFICATION_SERVICE);
+                    notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+
+
+                    final TextView myClickableUrl = (TextView) findViewById(R.id.textInstructions);
+                    myClickableUrl.setText("See your results in realtime: " + serverAddress + "/view/location/" + familyName + "/" + deviceName);
+                    Linkify.addLinks(myClickableUrl, Linkify.WEB_URLS);
+                } else {
+                    TextView rssi_msg = (TextView) findViewById(R.id.textOutput);
+                    rssi_msg.setText("not running");
+                    Log.d(TAG, "toggle set to false");
+                    alarms.cancel(recurringLl24);
+                    android.app.NotificationManager mNotificationManager = (android.app.NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                    mNotificationManager.cancel(0);
+                    timer.cancel();
+                }
+            }
+        });
+
+
+    }
+
+
+    private void connectWebSocket() {
+        URI uri;
+        try {
+            String serverAddress = ((EditText) findViewById(R.id.serverAddress)).getText().toString();
+            String familyName = ((EditText) findViewById(R.id.familyName)).getText().toString();
+            String deviceName = ((EditText) findViewById(R.id.deviceName)).getText().toString();
+            serverAddress = serverAddress.replace("http", "ws");
+            uri = new URI(serverAddress + "/ws?family=" + familyName + "&device=" + deviceName);
+            Log.d("Websocket", "connect to websocket at " + uri.toString());
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        mWebSocketClient = new WebSocketClient(uri) {
+            @Override
+            public void onOpen(ServerHandshake serverHandshake) {
+                Log.i("Websocket", "Opened");
+                mWebSocketClient.send("Hello");
+            }
+
+            @Override
+            public void onMessage(String s) {
+                final String message = s;
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Log.d("Websocket", "message: " + message);
+                        JSONObject json = null;
+                        JSONObject fingerprint = null;
+                        JSONObject sensors = null;
+                        JSONObject bluetooth = null;
+                        JSONObject wifi = null;
+                        String deviceName = "";
+                        String locationName = "";
+                        String familyName = "";
+                        try {
+                            json = new JSONObject(message);
+                        } catch (Exception e) {
+                            Log.d("Websocket", "json error: " + e.toString());
+                            return;
+                        }
+                        try {
+                            fingerprint = new JSONObject(json.get("sensors").toString());
+                            Log.d("Websocket", "fingerprint: " + fingerprint);
+                        } catch (Exception e) {
+                            Log.d("Websocket", "json error: " + e.toString());
+                        }
+                        try {
+                            sensors = new JSONObject(fingerprint.get("s").toString());
+                            deviceName = fingerprint.get("d").toString();
+                            familyName = fingerprint.get("f").toString();
+                            locationName = fingerprint.get("l").toString();
+                            Log.d("Websocket", "sensors: " + sensors);
+                        } catch (Exception e) {
+                            Log.d("Websocket", "json error: " + e.toString());
+                        }
+                        try {
+                            wifi = new JSONObject(sensors.get("wifi").toString());
+                            Log.d("Websocket", "wifi: " + wifi);
+                        } catch (Exception e) {
+                            Log.d("Websocket", "json error: " + e.toString());
+                        }
+                        try {
+                            bluetooth = new JSONObject(sensors.get("bluetooth").toString());
+                            Log.d("Websocket", "bluetooth: " + bluetooth);
+                        } catch (Exception e) {
+                            Log.d("Websocket", "json error: " + e.toString());
+                        }
+                        Log.d("Websocket", bluetooth.toString());
+                        Integer bluetoothPoints = bluetooth.length();
+                        Integer wifiPoints = wifi.length();
+                        Long secondsAgo = null;
+                        try {
+                            secondsAgo = fingerprint.getLong("t");
+                        } catch (Exception e) {
+                            Log.w("Websocket", e);
+                        }
+
+                        if ((System.currentTimeMillis() - secondsAgo)/1000 > 3) {
+                            return;
+                        }
+                        SimpleDateFormat sdf = new SimpleDateFormat("MMM dd HH:mm:ss");
+                        Date resultdate = new Date(secondsAgo);
+//                        String message = sdf.format(resultdate) + ": " + bluetoothPoints.toString() + " bluetooth and " + wifiPoints.toString() + " wifi points inserted for " + familyName + "/" + deviceName;
+                        String message = "1 second ago: added " + bluetoothPoints.toString() + " bluetooth and " + wifiPoints.toString() + " wifi points for " + familyName + "/" + deviceName;
+                        oneSecondTimer.resetCounter();
+                        if (locationName.equals("") == false) {
+                            message += " at " + locationName;
+                        }
+                        TextView rssi_msg = (TextView) findViewById(R.id.textOutput);
+                        Log.d("Websocket", message);
+                        rssi_msg.setText(message);
+
+                    }
+                });
+            }
+
+            @Override
+            public void onClose(int i, String s, boolean b) {
+                Log.i("Websocket", "Closed " + s);
+            }
+
+            @Override
+            public void onError(Exception e) {
+                Log.i("Websocket", "Error " + e.getMessage());
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        TextView rssi_msg = (TextView) findViewById(R.id.textOutput);
+                        rssi_msg.setText("cannot connect to server, fingerprints will not be uploaded");
+                    }
+                });
+            }
+        };
+        mWebSocketClient.connect();
+    }
+
+
+
+
+}
